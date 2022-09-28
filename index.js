5d3e7727937c5fbab3330877063483cf4466fa56
Paarit <paaritverma26@gmail.com>
2022-09-27 23:25:14 -0400

    updating the packages

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-import { without } from "lodash";
+import  {without}  from "../node_modules/lodash/without.js"
 
-console.log(without);
+var array = [1,2,3,4,5,6,7,8];
+console.log('answer:', without(array, 3));
